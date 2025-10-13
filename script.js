8afdc0461bdae8193647cde675a6e574c9e7e3bf
jacob-ivy <jsrebel64@gmail.com>
2025-10-13 16:27:29 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail() {
+    let parms = {
+        fname : document.getElementById("fname").value,
+        lname : document.getElementById("lname").value,
+        phone : document.getElementById("phone").value,
+        desc : document.getElementById("desc").value
+    }
+
+    emailjs.send("service_gwa68dc", "template_b5twp2v", parms).then(alert("Email Sent!"))
+}
