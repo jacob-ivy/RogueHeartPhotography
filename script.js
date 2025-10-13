bb97669b66499668bbee29ba66acc39181e3fa37
jacob-ivy <jsrebel64@gmail.com>
2025-10-13 16:46:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function sendMail() {
 
     emailjs.send("service_gwa68dc", "template_b5twp2v", parms).then(alert("Email Sent!"))
 }
+
+function clearValues () {
+    document.getElementById("fname").value = ""
+    document.getElementById("lname").value = ""
+    document.getElementById("phone").value = ""
+    document.getElementById("desc").value = ""
+}
